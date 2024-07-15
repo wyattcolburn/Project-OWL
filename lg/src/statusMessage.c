0ed026c43ecddc3b278403eb34ef06b177e0f0ef
Rushabh <rshah49@calpoly.edu>
2024-07-15 16:11:28 -0700

    [ADD] all helper functions for simple

--- lg/src/statusMessage.c
+++ lg/src/statusMessage.c
@@ -42,7 +42,7 @@ void gpio_init(int chip_handle);
 int spiHandle(int spiDev, int spiChannel, int spiBaud, int spiFlag);
 void printBuffer(const char *buffer, int len);
 int getStatus(int spi_handle, char opcode, char *response, int response_len);
-int busyCheck(void);
+void wait_on_busy(void);
 
 int chip_handle = 0;
 int spi_handle = 0;
@@ -133,43 +133,49 @@ int getStatus(int spi_handle, char opcode, char *response, int response_len) {
     printf("Tx Buffer: ");
 	printBuffer(txBuf, response_len);
 	//first check if busy line is low
-	if (busyCheck() == READY) {
+	wait_on_busy();
 	//send status OP Code
 		return lgSpiXfer(spi_handle, txBuf, response, response_len);
-	}
-	return 0;
+	
 }
 
 
 void gpio_init(int chip_handle){
 
 	int sx_nreset =  lgGpioClaimOutput(chip_handle,0, SX_NRESET_PIN, HIGH); //init high
-	if (sx_nreset <0 ) {
-		printf("Failure to init GPIO 26: error code %d\n", sx_nreset);
-	}
-	else {
-		printf("NSS INIT success\n");
-	}
+	(sx_nreset >= 0 ) ? puts("sx_nreset init") : puts("sx_nreset fail");
+	
+	/*if (sx_nreset <0 ) {*/
+		/*printf("Failure to init GPIO 26: error code %d\n", sx_nreset);*/
+	/*}*/
+	/*else {*/
+		/*printf("NSS INIT success\n");*/
+	/*}*/
 
 	int busy = lgGpioClaimInput(chip_handle, 0, 16); //GPIO 16 input
+	(busy >= 0) ? puts("busy init") : puts("busy fail");
+	
+	/*if (busy <0 ) {*/
+		/*printf("Failure to init GPIO 26: error code %d\n", busy);*/
+	/*}*/
+	/*else {*/
+		/*printf("BUSY INIT success\n");*/
+	/*}*/
 
-	if (busy <0 ) {
-		printf("Failure to init GPIO 26: error code %d\n", busy);
-	}
-	else {
-		printf("BUSY INIT success\n");
-	}
-
-	int busyCheck1 = busyCheck();
-	if (busyCheck1 == LOW) {
-		printf("busy line low, is ready");
-	}
-	else if (busyCheck1 == HIGH) {
-		printf("busy line high, not ready");
-	}
-	else {
-		printf("error code %d\n", busyCheck1);
-	}
+	int txIRQ = lgGpioClaimInput(chip_handle, 0, 6); //gpio 6 input
+	
+	(txIRQ >= 0) ? puts("tx init") : puts("tx fail");
+	
+	/*int busyCheck1 = busyCheck();*/
+	/*if (busyCheck1 == LOW) {*/
+		/*printf("busy line low, is ready");*/
+	/*}*/
+	/*else if (busyCheck1 == HIGH) {*/
+		/*printf("busy line high, not ready");*/
+	/*}*/
+	/*else {*/
+		/*printf("error code %d\n", busyCheck1);*/
+	/*}*/
 		
 }
 
@@ -186,12 +192,12 @@ void wait_on_busy(void){
 	}
 }
 
-void tx_mode(){
+void tx_mode(void){
   /*
   1) Go in to STDBY MODE, using SetStandby 
   2) Set packet type--SetPacketType
   3) Define  the RF frequency--SetRfFrequency
-  4) Define power ampl confi--SetPaConfig()
+  4) Define power ampl confi--SetPaConfig(), step 4 done in setTxParams
   5) Define output power and ramping time with command 
         SetTxParams()
   6) Define where the data payload will stored with command 
@@ -202,18 +208,58 @@ void tx_mode(){
 
   9) Define frame format with SetPacketParams()^2
   10) Map TxDone IRQ to a DIO (DIO 1,2,or 3)
-  11) Define a sync value: use command WriteReg() to write value to register via direct access
-  12) Set circuit in transmitter mode to start tranmsiion SetTx(), use param to enable Timeout
+  11) Define a sync value: use command WriteReg() to write value to register via direct access--NOt sure if this needed, Kev does not do this
+  12) Set circuit in transmitter mode to start tranmsiion SetTx(), use param to enable Timeout//
   13) Wait for IRQ TxDone  or timeout-> chip once finished will go into STDBY_RC mode
-  14) Clear IRQ TxDone Flag
+  14) Clear IRQ TxDone Flag--not sure 
   */
+	
+	uint8_t data[5] = {1,2,3,4,5};
+	uint8_t data_len = 5;
 
 	set_standby_mode();
 	set_packet_type(LORA_PKT_TYPE);
-	set_rf_frequency(2); //idk what frequency to run
+	set_rf_frequency(915000000); //idk what frequency to run
+	set_tx_params(22, SET_RAMP_3400U);
+	set_buffer_base_addr(0x00, 0x00); //why both zeros? 
+	write_buffer(0x00, data, data_len); //write to buffer
+										
+	config_modulation_params(LORA_SF_7, LORA_BW_500, LORA_CR_4_5, 0x00); //low data optimazation off
+	config_packet_params(12, PKT_EXPLICIT_HDR, data_len, PKT_CRC_OFF, STD_IQ_SETUP);
 
-  
+	set_dio_irq_params(0x03FF, 0x03FF, 0x0000, 0x000); //we are not using DIO2 or 3 yet so set them to zero
+	set_tx_mode(0x00); //why no timeout??
+	wait_on_TX_IRQ();
+	puts("tx has been finished");
+	//need to clear the irq flag??
+
+
+	
+
+
+
 }
+
+void wait_on_TX_IRQ(void) {
+
+	//waits for TX IRQ to go low, means tx done and clears IRQ flag
+	
+	int txStatus= lgGpioRead(chip_handle, BUSY_PIN); 
+	while (txStatus == HIGH) {
+		SLEEP_MS(1);
+	}
+}
+
+/*void clear_tx_irq(){*/
+
+	/*//not sure how to do this? Think I am clearing all the status*/
+	/*uint8_t NOP_buffer[2] = {NO_OPERATION, NO_OPERATION};*/
+	/*uint16_t  tx_irq_status = read_registers(GET_IRQ_STATUS_OP, NOP_buffer, 2);*/
+	
+	/*tx_irq_status &= ~TX_DONE_MASK;*/
+
+	/*write_registers(CLEAR_IRQ_STATUS_OP, (uint8_t *)tx_irq_status, 1);*/
+/*}	*/
 void set_standby_mode(){
 	//0 for RC
 	//1 for XOCX
@@ -244,8 +290,8 @@ void set_rf_frequency(uint32_t frequency_mhz){
 }
 
 void set_pa_config(uint8_t pa_duty_cycle, uint8_t hp_max, uint8_t device_sel){
-
-	if(pa_duty_cycle > 0x04)
+//page 77 of data sheet
+	if(pa_duty_cycle > 0x04) //this is above the spec, should not happen 
         return; // Do nothing in this case
 
     if(hp_max > 0x07) // Clamp to the recommended max as per datasheet
@@ -274,7 +320,7 @@ void set_tx_params(int8_t power, uint8_t ramp_time){
 		power = 22;
 	}
 	uint8_t ocp_setting = OCP_SX1262_140_MA;
-	write_registers(REG_OCP_CONFIG, (const char *) &ocp_setting, 1); //whhy &
+	write_registers(REG_OCP_CONFIG, (char *) &ocp_setting, 1); //whhy &
 	
 	if (ramp_time > SET_RAMP_3400U) {
 		ramp_time = SET_RAMP_3400U;
@@ -282,8 +328,79 @@ void set_tx_params(int8_t power, uint8_t ramp_time){
 	uint8_t tx_config_buff[2] = {power, ramp_time};
 	sendCommand(spi_handle, SET_TX_PARAM_OP, tx_config_buff, 2);
 }
+void set_buffer_base_addr(uint8_t tx_base_addr, uint8_t rx_base_addr){
+	uint8_t addr_buff[2] = {tx_base_addr, rx_base_addr};
 
+	sendCommand(spi_handle, SET_BUFFER_BASE_ADDR_OP, addr_buff, 2);
+}
+uint8_t write_buffer(uint8_t offset, uint8_t* data, uint16_t len){
 
+	uint8_t status;
+	uint8_t cmd_offset[2] = {WRITE_BUF_OP, offset};
+
+	int spiWrite = lgSpiWrite(spi_handle, (const char *) cmd_offset, 2);
+	int spiWrite2 = lgSpiWrite(spi_handle, (const char *) data, len - 1);
+	int spiReadWrite = lgSpiXfer(spi_handle, (const char *) data + (len +1 ), (char *) &status, 1);
+
+	wait_on_busy();
+
+	return status;
+}
+
+void config_modulation_params(uint8_t spreading_factor, uint8_t bandwidth, uint8_t coding_rate, uint8_t low_data_rate_opt){
+
+	uint8_t mod_params[4] = {spreading_factor, bandwidth, coding_rate, low_data_rate_opt};
+
+	sendCommand(spi_handle, SET_MOD_PARAM_OP, mod_params, 4);
+
+}
+
+void config_packet_params(uint8_t preamble_length, uint8_t header_type, uint8_t payload_length, uint8_t crc_type, uint8_t invert_iq) {
+
+	uint8_t packet_param_buff[6];
+
+	packet_param_buff[0] = (uint8_t)((preamble_length >>8 ) & 0x00FF);
+	packet_param_buff[1] = (uint8_t)((preamble_length & 0x00FF));
+	packet_param_buff[2] = (header_type == IMPLICIT_HDR) ? IMPLICIT_HDR : EXPLICIT_HDR;
+	packet_param_buff[3] = payload_length;
+	packet_param_buff[4] = (crc_type == CRC_ON) ? CRC_ON : CRC_OFF;
+	packet_param_buff[5] = (invert_iq == INV_IQ_SETUP) ? INV_IQ_SETUP : STD_IQ_SETUP;
+
+	sendCommand(spi_handle, SET_PACKET_PARAMS_OP, packet_param_buff, 6);
+}
+
+void set_dio_irq_params(uint16_t irq_mask, uint16_t dio1_mask, uint16_t dio2_mask,
+						uint16_t dio3_mask) {
+
+	uint8_t irq_dio_buff[8];
+
+	 // IRQ Mask
+    irq_dio_buff[0] = (uint8_t) ((irq_mask >> 8) & 0x00FF);
+    irq_dio_buff[1] = (uint8_t) (irq_mask & 0x00FF);
+
+    // DIO1 Mask
+    irq_dio_buff[2] = (uint8_t) ((dio1_mask >> 8) & 0x00FF);
+    irq_dio_buff[3] = (uint8_t) (dio1_mask & 0x00FF);
+
+    // DIO2 Mask
+    irq_dio_buff[4] = (uint8_t) ((dio2_mask >> 8) & 0x00FF);
+    irq_dio_buff[5] = (uint8_t) (dio2_mask & 0x00FF);
+    
+    // DIO3 Mask
+    irq_dio_buff[6] = (uint8_t) ((dio3_mask >> 8) & 0xFF);
+    irq_dio_buff[7] = (uint8_t) (dio3_mask & 0x00FF);
+
+    sendCommand(spi_handle, SET_DIO_IRQ_PARMS_OP, irq_dio_buff, 8);
+}
+
+void set_tx_mode(uint32_t timeout) {
+	uint8_t timeout_buff[3];
+    timeout_buff[0] = (uint8_t) (timeout >> 16) & 0xFF;
+    timeout_buff[1] = (uint8_t) (timeout >> 8) & 0xFF;
+    timeout_buff[2] = (uint8_t) timeout & 0xFF;
+
+	sendCommand(spi_handle, SET_TX_MODE_OP, timeout_buff, 3);
+}
 
 uint8_t write_registers(uint16_t reg_addr, uint8_t * data, uint8_t len) {
 	//ISSUES MIGHT ARISE WITH NSS going high and low through multiple spi operations
@@ -313,9 +430,6 @@ uint8_t read_registers(uint16_t reg_addr, uint8_t* data, uint8_t len){
 
 	return status;
 }
-
-
-
 
 
 //do we want to have this return a value if the message goes through??
--- lg/src/statusMessage.h
+++ lg/src/statusMessage.h
@@ -5,7 +5,7 @@
 
 #define BUSY_PIN 16
 #define SX_NRESET_PIN 26
-
+#define TX_PIN 
 #define LOW 0
 #define HIGH 1
 
@@ -30,11 +30,25 @@ void set_standby_mode();
 void set_packet_type(uint8_t packet_type);
 void set_rf_frequency(uint32_t frequency_mhz);
 void set_pa_config(uint8_t pa_duty_cycle, uint8_t hp_max, uint8_t device_sel);
-void set_tx_config(int8_t power, uint8_t ramp_time);
+void set_tx_params(int8_t power, uint8_t ramp_time);
 
 uint8_t read_registers(uint16_t reg_addr, uint8_t* data, uint8_t len);
 
+void set_buffer_base_addr(uint8_t tx_base_addr, uint8_t rx_base_addr);
 
 uint8_t write_registers(uint16_t reg_addr, uint8_t* data, uint8_t len);
 
-#endif 
+
+uint8_t write_buffer(uint8_t offset, uint8_t* data, uint16_t len);
+
+void config_modulation_params(uint8_t spreading_factor, uint8_t bandwidth, uint8_t coding_rate, uint8_t low_data_rate_opt);
+void config_packet_params(uint8_t preamble_length, uint8_t header_type, uint8_t payload_length, uint8_t crc_type, uint8_t invert_iq);
+void set_dio_irq_params(uint16_t irq_mask, uint16_t dio1_mask, uint16_t dio2_mask,
+						uint16_t dio3_mask);
+
+void set_tx_mode(uint32_t timeout);
+void wait_on_TX_IRQ(void);
+void tx_mode(void);
+
+#endif
+
