14aed7a60f924e5d85afe2c0bfb3fee891216f4e
wyattcolburn <wyattdcolburn@gmail.com>
2024-08-05 15:34:02 -0700

    removed lg dependecies and wrote install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install unzip
+
+
+git clone https://github.com/wyattcolburn/Project-OWL.git
+git clone https://github.com/wyattcolburn/Project-OWL.git
+cd Project-OWL
+cd lg
+make
+sudo make install
+cd ..
+sudo rm -rf lg 
+
