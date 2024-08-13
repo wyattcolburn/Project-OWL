69e5b501cfeba8c919aa8e6b076606aff425fa5a
wyattcolburn <wyattdcolburn@gmail.com>
2024-08-13 21:53:39 +0100

    look at redis.c for func to implment into your work

---
+++ src/messageBroker/redis.h
@@ -0,0 +1,14 @@
+#ifndef REDIS_H
+#define REDIS_H
+#include <hiredis/hiredis.h>
+
+
+redisContext * redis_init(char * server, int port);
+void publish(redisContext * redisConnect, const char *stream_name, const char * key,
+char *value, char * response);
+void readStream(redisContext * redis_connect, const char * stream_name, char * response);
+
+
+#endif
+
+
