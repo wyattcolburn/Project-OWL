d016dad00ba0ea1468bdc91f7a7f5309973c13d9
wyattcolburn <wyattdcolburn@gmail.com>
2024-08-26 17:17:18 -0700

    CDP stuff with cmake

---
+++ src/CDP/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/BloomFilter.cpp" "CMakeFiles/main.dir/src/BloomFilter.cpp.o" "gcc" "CMakeFiles/main.dir/src/BloomFilter.cpp.o.d"
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/Packet.cpp" "CMakeFiles/main.dir/src/Packet.cpp.o" "gcc" "CMakeFiles/main.dir/src/Packet.cpp.o.d"
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/PapaDuck.cpp" "CMakeFiles/main.dir/src/PapaDuck.cpp.o" "gcc" "CMakeFiles/main.dir/src/PapaDuck.cpp.o.d"
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/Utils.cpp" "CMakeFiles/main.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/main.dir/src/Utils.cpp.o.d"
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "/home/wyattcolburn/Project-OWL/src/CDP/src/redis.cpp" "CMakeFiles/main.dir/src/redis.cpp.o" "gcc" "CMakeFiles/main.dir/src/redis.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CDP/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/BloomFilter.cpp.o"
+  "CMakeFiles/main.dir/src/BloomFilter.cpp.o.d"
+  "CMakeFiles/main.dir/src/Packet.cpp.o"
+  "CMakeFiles/main.dir/src/Packet.cpp.o.d"
+  "CMakeFiles/main.dir/src/PapaDuck.cpp.o"
+  "CMakeFiles/main.dir/src/PapaDuck.cpp.o.d"
+  "CMakeFiles/main.dir/src/Utils.cpp.o"
+  "CMakeFiles/main.dir/src/Utils.cpp.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/redis.cpp.o"
+  "CMakeFiles/main.dir/src/redis.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
