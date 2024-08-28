1f173ab3d13d1707b24fb75fc9e0a3153a1afbd2
wyattcolburn <wyattdcolburn@gmail.com>
2024-08-28 12:42:51 -0700

    fix

--- src/utils/rx/receive_oneshot.c
+++ src/utils/rx/receive_oneshot.c
@@ -4,7 +4,7 @@
 #include "transmit.h"
 #include "helpFunctions.h"
 #include "sx1262x_defs_custom.h"
-#include "../header_files/helpFunctions.h"
+#include "helpFunctions.h"
 
 #define GET_STATUS_OP                   UINT8_C(0xC0)
 #define GET_STATS_OP                    UINT8_C(0x10)
